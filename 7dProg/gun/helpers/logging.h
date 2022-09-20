46fc5e97d111a1ea68a586a9d2e854ff4292a0f7
Tombleron <fokindacer@gmail.com>
2022-09-20 14:54:17 +0700

    Edit: slightly improved hellpers

--- 7dProg/gun/helpers/logging.h
+++ 7dProg/gun/helpers/logging.h
@@ -1,14 +1,14 @@
 
 #ifdef DEBUG
 #define DBG(a, ...)                                                            \
-  { printf(a "\n", ##__VA_ARGS__); }
+  { printf("[DEBUG]: " a "\n", ##__VA_ARGS__); }
 #else
 #define DBG(a, ...)                                                            \
   {}
 #endif
 
 #define INFO(a, ...)                                                           \
-  { printf("[INFO]: " a "\n", __VA_ARGS__); }
+  { printf("[INFO]: " a "\n", ##__VA_ARGS__); }
 
 #define ERROR(a, ...)                                                          \
   { printf("[ERROR]: " a "\n", ##__VA_ARGS__); }
